323bed21605a4dea244b2762e128e31ea166d39b
Ankit Virani <ankit.virani9393@gmail.com>
2022-04-18 09:57:02 +0530

    Update task_1.kt

--- task_1.kt
+++ task_1.kt
@@ -1,34 +1,13 @@
 fun main(args: Array<String>) {
         
-    	val trains = mapOf(
-            '0' to mutableListOf("Bandra Tarminus Palitana Superfast Express ", "Surat", "Palitana", "21:30"),
-            '1' to mutableListOf("Andhra Pradesh Express", "New Delhi", "Andhra Pradesh", "02:10"),
-    		'2' to mutableListOf("Surat To Cochin", "Surat", "Cochin", "15:00"),
-        '3' to mutableListOf("MUMBAI CENTRAL - JAIPUR SF Express", "Surat", "Jaipur", "10:00"),
-        )
-                
-       	val getIndex: Int = when(args[0]) {
-            "name" -> 0
-            "source" -> 1
-            "destination" -> 2
-            "time" -> 3
-            else -> -1
-        }		
-        
-        var result: Boolean = false
-    	for ((key, value) in trains) {
-            result = getTrains(args[1], getIndex, value)
-            if(result) {
-             	println("Train Details : $value")
-            }
-        }    
-   }
-
-fun getTrains(value: String, index: Int, train: MutableList<String>): Boolean {
-    for (arrIndex in train.indices) {
-        if(arrIndex === index && train[arrIndex].uppercase() == value.uppercase()) {
-        	return true
-        }
+    	val trains = arrayListOf(
+        arrayListOf<String>("Bandra Tarminus Palitana Superfast Express", "Surat", "Palitana", "21:30"),
+        arrayListOf<String>("Andhra Pradesh Express", "New Delhi", "Andhra Pradesh", "02:10"),
+        arrayListOf<String>("Surat To Cochin", "Surat", "Cochin", "15:00"),
+        arrayListOf<String>("Mumbai Central Jaipur SF Express", "Surat", "Jaipur", "10:00")    
+    )
+    val result = trains.filter{
+        it[0] == args[0] || it[1] == args[0] || it[2] == args[0] || it[3] == args[0]   
     }
-    return false
+    println(result)
 }
